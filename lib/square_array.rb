725f4b63a93de5ee27568fc308e8e62c774b1a46
Eddie Lopez <iameddielopez@gmail.com>
2020-07-15 15:49:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,12 @@ array = [numbers]
 counter = 0
 
 def square_array(array)
-  while array
+  while array[counter] do
+    array**2[counter]
+    counter+=1 
+  end
+end
+
  
 while array.length.times do |index|
   intergers**2
