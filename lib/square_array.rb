daedcb8a482af526832e2addd86eb61fd2fec0c3
Eddie Lopez <iameddielopez@gmail.com>
2020-07-15 16:59:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,9 +3,10 @@
   
 def square_array(array)
   counter = 0
+  new_numbers = []
   
   while counter < array.length do
-    array**2 = new_numbers
+    array**2 << new_numbers
     counter += 1 
   end
 end
