ca6f6726f321efa442aa5499028d49c5a346b7a3
Eddie Lopez <iameddielopez@gmail.com>
2020-07-15 17:25:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,7 +9,8 @@ def square_array(array)
   while counter < array.length do
    # binding.pry
     # array[counter]**2 << new_numbers
-    #new_numbers << array[counter] ** 2
+    #new_numbers << array[counter] ** 2 can be done both ways make sure array you are pushing into is on the left side
+  
     new_numbers.push(array[counter] ** 2 )
     counter += 1 
   end
