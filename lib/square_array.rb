eaea3d9e0c1c58424090a8221493e9b3a08ce52a
Eddie Lopez <iameddielopez@gmail.com>
2020-07-15 17:09:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,13 @@
 
   # your code here
+  require 'pry'
   
 def square_array(array)
   counter = 0
   new_numbers = []
   
   while counter < array.length do
+    binding.pry
     array**2 << new_numbers
     counter += 1 
   end
